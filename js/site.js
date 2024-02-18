9f2ee6a745b7fb0419c6a0be86beca888777e7cd
Viv-dn <Vivian96@live.se>
2024-02-18 23:34:39 +0100

    index

--- js/site.js
+++ js/site.js
@@ -1,11 +1,31 @@
 //visa listan av producter
 
-// var foods = [
-//   { id: 1, name: "Pho" },
-//   { id: 2, name: "Bun Cha" },
-//   { id: 3, name: "Banh Xeo" },
-//   { id: 4, name: "Banh Cuon" },
-// ];
+var foods = [
+  {
+    id: 1,
+    name: "Pho",
+    description: `Vietnamese rice noodle soup with coriander, chive, sweet basil, bean sprouts. 
+    Served with beef or chicken.`,
+  },
+  {
+    id: 2,
+    name: "Bun Cha",
+    description: `Grilled pork meat marinated with vietnamese spices.
+    Served with rice noodles and special fish sauce.`,
+  },
+  {
+    id: 3,
+    name: "Banh Xeo",
+    description: `Vietnamese crêpes stuffed with shrimp, pork belly and bean sprouts. 
+    Served with lettuce, sweet basil, mint and special fish sauce.`,
+  },
+  {
+    id: 4,
+    name: "Banh Cuon",
+    description: `Thinly steamed rice rolls filled with a mixture of cooked ground pork, mushroom, roasted shallots.
+    Served with special fish sauce and vietnamese sasauge.`,
+  },
+];
 
 //visa kundvagn
 
@@ -13,13 +33,16 @@ let cart = [];
 
 //lägga till och ta bort saker från kundvagn
 
-function addToCart(productName, productId) {
-  var food = {
-    id: productId,
-    name: productName,
-  };
-  cart.push(food);
+function addToCart(foods) {
+  const food = cart.find((item) => item.name === foods.name);
+
+  cart.push({
+    id: foods.id,
+    name: foods.name,
+    description: foods.description,
+  });
   console.log(cart);
+  updateCartDisplay();
 }
 
 var addToCartBtn = Document.getElementById("addToCartBtn");
@@ -35,10 +58,15 @@ function updateCartDisplay() {
   cartItems.innerText = "";
 
   cart.forEach(function (foods) {
-    const li = document.createElement(`li`);
+    const li = document.createElement("li");
     li.innerHTML = `<a class="dropdown-item" href="#">` + foods.name + `</a>`;
     cartItems.appendChild(li);
   });
+}
+
+function removeFromCart(prod) {
+  cart.splice(prod, 1);
+  updateCartDisplay();
 }
 
 //event
